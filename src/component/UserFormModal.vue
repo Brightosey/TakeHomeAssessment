703ccc5fd47fdcb8f79cd63fd8cb5ad0bc15dfa5
Brightosey <Brightosey@users.noreply.github.com>
2025-09-18 07:13:57 -0400

    Update: Fixed styling and added functionality to the create user button

--- src/component/UserFormModal.vue
+++ src/component/UserFormModal.vue
@@ -1,22 +1,30 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import { storeToRefs } from "pinia";
 import useUserStore from "../store/users";
 
 const emit = defineEmits(["close"]);
 
 const userStore = useUserStore();
-
+const { users } = storeToRefs(userStore);
 const newUser = ref({
   first_name: "",
   last_name: "",
   email: "",
   plan: "",
   company: "",
-  phone: "",
+  phone_number: "",
+});
+const isLoading = ref(false);
+
+const companies = computed(() => {
+  return [...new Set(users.value.map((user) => user.company))];
 });
 
 const saveNewUser = async () => {
+  isLoading.value = true;
   await userStore.createUser(newUser.value);
+  isLoading.value = false;
   emit("close");
 };
 </script>
@@ -24,7 +32,9 @@ const saveNewUser = async () => {
 <template>
   <div class="modal-overlay" @click.self="emit('close')">
     <div class="modal">
-      <h3>Create New User</h3>
+      <h2>Create New User</h2>
+      <br />
+      <p class="close" @click="emit('close')">x</p>
       <form @submit.prevent="saveNewUser">
         <input v-model="newUser.first_name" placeholder="First Name" required />
         <input v-model="newUser.last_name" placeholder="Last Name" required />
@@ -44,16 +54,23 @@ const saveNewUser = async () => {
         </select>
 
         <select v-model="newUser.company" required>
-          <option>Apple</option>
-          <option>Microsoft</option>
-          <option>Google</option>
-          <option>Axiom</option>
+          <option value="">Select Company</option>
+          <option v-for="company in companies" :key="company" :value="company">
+            {{ company }}
+          </option>
         </select>
 
-        <input v-model="newUser.phone" placeholder="Phone #" required />
+        <input
+          v-model="newUser.phone_number"
+          type="number"
+          placeholder="Phone #"
+          required
+        />
 
         <div class="actions">
-          <button type="submit" @click="emit('close')">Save</button>
+          <button type="submit" @submit="saveNewUser">
+            {{ isLoading ? "Saving..." : "Save" }}
+          </button>
         </div>
       </form>
     </div>
@@ -67,7 +84,7 @@ const saveNewUser = async () => {
   left: 0;
   width: 100%;
   height: 100vh;
-  background: rgba(255, 255, 255, 0.3);
+  background: rgba(0, 0, 0, 0.436);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -77,8 +94,9 @@ const saveNewUser = async () => {
   background: #fff;
   padding: 1.5rem;
   border-radius: 6px;
-  width: 90%;
-  height: 70vh;
+  width: 500px;
+  height: fit-content;
+  position: relative;
 }
 
 form {
@@ -87,16 +105,37 @@ form {
   gap: 1rem;
 }
 
+.close {
+  position: absolute;
+  font-family: Helvetica;
+  top: 10px;
+  right: 20px;
+  cursor: pointer;
+  font-size: 20px;
+}
+
 .actions {
   display: flex;
   justify-content: flex-end;
   gap: 0.5rem;
 }
+
+input,
+select {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 0.5rem;
+  font-size: 14px;
+  width: 100%;
+  appearance: none;
+}
+
 .actions button {
   padding: 0.5rem 1rem;
   border: none;
   border-radius: 4px;
   cursor: pointer;
+  margin-top: 1rem;
 }
 .actions button:first-child {
   background: #ccc;
